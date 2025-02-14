a1c6dd49926dad9899248e7f83a8e88a04dadf6a
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-14 11:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+let row = document.createElement('tr');
+let cell = document.createElement('td');
+cell.colSpan = "2";
+cell.innerText = `Total Price: ${sum}`;
+row.appendChild(cell);
+
+document.querySelector('table').appendChild(row);
   
 };
 
